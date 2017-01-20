2a22a4b0e2205ba4f7d8b707677a21aa7be7032a
John Petrocik <john@petrocik.net>
2017-01-19 21:59:55 -0800

    Upgrade to post

--- mythtv.js
+++ mythtv.js
@@ -5,50 +5,52 @@ function MythTv() {
 		frontendPort = "6547";
 
 
-	function getRequest(url, port, callback) {
+	function sendRequest(method, url, port, callback) {
 		var options = {
-		host: frontendIp,
-		port: port,
-		path: url,
-		headers: {
-			Accept: 'application/json',
-		}
+			host: frontendIp,
+			port: port,
+			path: url,
+			method: method,
+			headers: {
+				Accept: 'application/json',
+			}
 		};
 
 		console.log(url);
-		return http.get(options, function(response) {
-		var body = '';
-		response.on('data', function(d) {
-			body += d;
-		});
-		response.on('end', function() {
-			if (response.statusCode == 200) {
-				var results = JSON.parse(body);
-				callback(response.statusCode, results);
-			} else {
-				callback(response.statusCode);
-			}
-		});
+
+		return http.request(options, function(response) {
+			var body = '';
+			response.on('data', function(d) {
+				body += d;
+			});
+			response.on('end', function() {
+				if (response.statusCode == 200) {
+					var results = JSON.parse(body);
+					callback(response.statusCode, results);
+				} else {
+					callback(response.statusCode);
+				}
+			});
 		});
 	};
 
 	this.stop = function(callback) {
 		console.log("Stop Playback");
-		getRequest("/Frontend/SendAction?Action=STOPPLAYBACK", 6547, function(statusCode, results){
+		sendRequest("POST", "/Frontend/SendAction?Action=STOPPLAYBACK", 6547, function(statusCode, results){
 			callback(statusCode === 200);
 		});
 	};
 
 	this.pause = function(callback) {
 		console.log("Pausing Playback");
-		getRequest("/Frontend/SendAction?Action=PAUSE", 6547, function(statusCode, results){
+		sendRequest("POST", "/Frontend/SendAction?Action=PAUSE", 6547, function(statusCode, results){
 			callback(statusCode === 200);
 		});
 	};
 
 	this.resume = function(callback) {
 		console.log("Resuming Playback");
-		getRequest("/Frontend/SendAction?Action=PLAY", 6547, function(statusCode, results){
+		sendRequest("POST", "/Frontend/SendAction?Action=PLAY", 6547, function(statusCode, results){
 		callback(statusCode === 200);
 		});
 	};
@@ -67,7 +69,7 @@ function MythTv() {
 				var foundMatch = results.ProgramList.Programs.some(function(program, index){
 					if ( program.Title.toLowerCase() === show.toLowerCase()) {
 						console.log("Starting " + program.Title);
-						getRequest("/Frontend/PlayRecording?ChanId=" + program.Channel.ChanId + "&StartTime=" + program.StartTime, 6547, 
+						sendRequest("POST", "/Frontend/PlayRecording?ChanId=" + program.Channel.ChanId + "&StartTime=" + program.StartTime, 6547, 
 							function(statusCode, results) {
 								callback(statusCode === 200);
 							}
@@ -88,7 +90,7 @@ function MythTv() {
 
 	this.recordedListing = function(callback) {
 		console.log("Retrieving record shows");
-		getRequest("/Dvr/GetRecordedList?StartIndex=1&Count=100&Descending=true", 6544, callback);
+		sendRequest("GET", "/Dvr/GetRecordedList?StartIndex=1&Count=100&Descending=true", 6544, callback);
 	}
 }
 
